17feb929bd9b45eb657e2939dcb875131e029b5c
Ravi Tyagi <tyagiravikumar@gmail.com>
2017-07-22 09:41:38 -0700

    updated

--- ngInventory/src/app/pages/pages.menu.ts
+++ ngInventory/src/app/pages/pages.menu.ts
@@ -18,51 +18,31 @@ export const PAGES_MENU = [
         path: 'editors',
         data: {
           menu: {
-            title: 'general.menu.editors',
+            title: 'general.menu_inv.invoice',
             icon: 'ion-edit',
             selected: false,
             expanded: false,
             order: 100,
           }
-        },
-        children: [
-          {
-            path: 'ckeditor',
-            data: {
-              menu: {
-                title: 'general.menu.ck_editor',
-              }
-            }
-          }
-        ]
+        }
       },
       {
         path: 'components',
         data: {
           menu: {
-            title: 'general.menu.components',
+            title: 'general.menu_inv.inventory',
             icon: 'ion-gear-a',
             selected: false,
             expanded: false,
             order: 250,
           }
-        },
-        children: [
-          {
-            path: 'treeview',
-            data: {
-              menu: {
-                title: 'general.menu.tree_view',
-              }
-            }
-          }
-        ]
+        }
       },
       {
         path: 'charts',
         data: {
           menu: {
-            title: 'general.menu.charts',
+            title: 'general.menu_inv.reports',
             icon: 'ion-stats-bars',
             selected: false,
             expanded: false,
@@ -84,7 +64,7 @@ export const PAGES_MENU = [
         path: 'ui',
         data: {
           menu: {
-            title: 'general.menu.ui_features',
+            title: 'general.menu_inv.contactus',
             icon: 'ion-android-laptop',
             selected: false,
             expanded: false,
@@ -136,218 +116,11 @@ export const PAGES_MENU = [
             path: 'grid',
             data: {
               menu: {
-                title: 'general.menu.grid',
-              }
-            }
-          },
-        ]
-      },
-      {
-        path: 'forms',
-        data: {
-          menu: {
-            title: 'general.menu.form_elements',
-            icon: 'ion-compose',
-            selected: false,
-            expanded: false,
-            order: 400,
-          }
-        },
-        children: [
-          {
-            path: 'inputs',
-            data: {
-              menu: {
-                title: 'general.menu.form_inputs',
-              }
-            }
-          },
-          {
-            path: 'layouts',
-            data: {
-              menu: {
-                title: 'general.menu.form_layouts',
+                title: 'general.menu.grid'
               }
             }
           }
         ]
-      },
-      {
-        path: 'tables',
-        data: {
-          menu: {
-            title: 'general.menu.tables',
-            icon: 'ion-grid',
-            selected: false,
-            expanded: false,
-            order: 500,
-          }
-        },
-        children: [
-          {
-            path: 'basictables',
-            data: {
-              menu: {
-                title: 'general.menu.basic_tables',
-              }
-            }
-          },
-          {
-            path: 'smarttables',
-            data: {
-              menu: {
-                title: 'general.menu.smart_tables',
-              }
-            }
-          },
-          {
-            path: 'datatables',
-            data: {
-              menu: {
-                title: 'Data Tables',
-              }
-            }
-          },
-           {
-             path: 'hottables',
-             data: {
-               menu: {
-                 title: 'Hot Tables',
-               }
-             }
-           }
-        ]
-      },
-      {
-        path: 'maps',
-        data: {
-          menu: {
-            title: 'general.menu.maps',
-            icon: 'ion-ios-location-outline',
-            selected: false,
-            expanded: false,
-            order: 600,
-          }
-        },
-        children: [
-          {
-            path: 'googlemaps',
-            data: {
-              menu: {
-                title: 'general.menu.google_maps',
-              }
-            }
-          },
-          {
-            path: 'leafletmaps',
-            data: {
-              menu: {
-                title: 'general.menu.leaflet_maps',
-              }
-            }
-          },
-          {
-            path: 'bubblemaps',
-            data: {
-              menu: {
-                title: 'general.menu.bubble_maps',
-              }
-            }
-          },
-          {
-            path: 'linemaps',
-            data: {
-              menu: {
-                title: 'general.menu.line_maps',
-              }
-            }
-          }
-        ]
-      },
-      {
-        path: '',
-        data: {
-          menu: {
-            title: 'general.menu.pages',
-            icon: 'ion-document',
-            selected: false,
-            expanded: false,
-            order: 650,
-          }
-        },
-        children: [
-          {
-            path: ['/login'],
-            data: {
-              menu: {
-                title: 'general.menu.login'
-              }
-            }
-          },
-          {
-            path: ['/register'],
-            data: {
-              menu: {
-                title: 'general.menu.register'
-              }
-            }
-          }
-        ]
-      },
-      {
-        path: '',
-        data: {
-          menu: {
-            title: 'general.menu.menu_level_1',
-            icon: 'ion-ios-more',
-            selected: false,
-            expanded: false,
-            order: 700,
-          }
-        },
-        children: [
-          {
-            path: '',
-            data: {
-              menu: {
-                title: 'general.menu.menu_level_1_1',
-                url: '#'
-              }
-            }
-          },
-          {
-            path: '',
-            data: {
-              menu: {
-                title: 'general.menu.menu_level_1_2',
-                url: '#'
-              }
-            },
-            children: [
-              {
-                path: '',
-                data: {
-                  menu: {
-                    title: 'general.menu.menu_level_1_2_1',
-                    url: '#'
-                  }
-                }
-              }
-            ]
-          }
-        ]
-      },
-      {
-        path: '',
-        data: {
-          menu: {
-            title: 'general.menu.external_link',
-            url: 'http://www.rnksystem.com',
-            icon: 'ion-android-exit',
-            order: 800,
-            target: '_blank'
-          }
-        }
       }
     ]
   }
